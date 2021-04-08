e301d52bb9665095e2893054e5b0cc04992df6f4
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-09 00:44:10 +0200

    scope-mistake-2 comment

--- describing-errors/exercises/reference-error/scope-mistake-2.js
+++ describing-errors/exercises/reference-error/scope-mistake-2.js
@@ -1,27 +1,35 @@
-'use strict';
+"use strict";
 
 /*
-  browser:
+  browser: Chrome
 
-  name:
-  message:
+  name: ReferenceError
+  message: isValidUserName is not defined
 
-  callstack:
+  callstack: 
+    at <anonymous>:29:57
+    at HTMLIFrameElement.evaller.onload (study-with.js:40)
+    at Object.debugger (study-with.js:42)
+    at JavaScriptFE.studyWith (javascript-class.js:303)
+    at HTMLButtonElement.<anonymous> (javascript-class.js:149)
 
-  life cycle:
+  life cycle: execution phase
 
-  the mistake:
+  the mistake: the variable isValidUserName was declared inside of scope, so
+  it can not be read outside the scope, by console.
 
-  the fix(es):
+  the fix(es): to insert console.log call inside the scope, so that variable could be read.
 */
 
-const userName = 'chiobin';
-console.log('userName:', typeof userName, userName);
+const userName = "chiobin";
+console.log("userName:", typeof userName, userName);
 
 if (userName.length > 4) {
   const isValidUserName = true;
+  console.log("isValidUserName:", typeof isValidUserName, isValidUserName);
 } else {
   const isValidUserName = false;
+  console.log("isValidUserName:", typeof isValidUserName, isValidUserName);
 }
 
-console.log('isValidUserName:', typeof isValidUserName, isValidUserName);
+//console.log("isValidUserName:", typeof isValidUserName, isValidUserName); //error
