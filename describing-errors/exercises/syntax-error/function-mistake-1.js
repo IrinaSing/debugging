9c160445f0945afb8b5837a852ba523776d7e650
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-10 23:43:10 +0200

    function-mistakes

--- describing-errors/exercises/syntax-error/function-mistake-1.js
+++ describing-errors/exercises/syntax-error/function-mistake-1.js
@@ -1,22 +1,27 @@
-'use strict';
+"use strict";
 
 /*
   browser:
 
-  name:
-  message:
+  name: SyntaxError
+  message: Unexpected token '{'
 
   callstack:
+    at HTMLIFrameElement.evaller.onload (study-with.js:40)
+    at Object.debugger (study-with.js:42)
+    at JavaScriptFE.studyWith (javascript-class.js:303)
+    at HTMLButtonElement.<anonymous> (javascript-class.js:149)
 
-  life cycle:
+  life cycle: creation phase
 
-  the mistake:
+  the mistake: "=>" fat arrow expected in a function, because this is an Arrow function.
 
-  the fix(es):
+  the fix(es): add "=>" fat arrow
 */
 
-const add = (x = 0, y = 0){
+const add = (x = 0, y = 0) => {
   return x + y;
 };
 
 const seven = add(3, 4);
+console.log(seven);
--- describing-errors/exercises/syntax-error/function-mistake-2.js
+++ describing-errors/exercises/syntax-error/function-mistake-2.js
@@ -1,22 +1,27 @@
-'use strict';
+"use strict";
 
 /*
-  browser:
+  browser: Chrome
 
-  name:
-  message:
+  name: SyntaxError:
+  message: Malformed arrow function parameter list
 
   callstack:
+    at HTMLIFrameElement.evaller.onload (study-with.js:40)
+    at Object.debugger (study-with.js:42)
+    at JavaScriptFE.studyWith (javascript-class.js:303)
+    at HTMLButtonElement.<anonymous> (javascript-class.js:149)
 
-  life cycle:
+  life cycle: creation phase
 
-  the mistake:
+  the mistake: parameter list of the Function is not surrounded with parenthesis.
 
-  the fix(es):
+  the fix(es): Bring function to format Function().
 */
 
-const add = x = 0, y = 0 => {
+const add = (x = 0, y = 0) => {
   return x + y;
 };
 
 const seven = add(3, 4);
+console.log(seven);
