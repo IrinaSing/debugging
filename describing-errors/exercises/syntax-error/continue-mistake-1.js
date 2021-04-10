19a2a275fe4b3c7e6ba4795fed13ca2e98a5081a
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-10 23:16:34 +0200

    continue-mistakes

--- describing-errors/exercises/syntax-error/continue-mistake-1.js
+++ describing-errors/exercises/syntax-error/continue-mistake-1.js
@@ -1,23 +1,26 @@
-'use strict';
+"use strict";
 
 /*
-  browser:
+  browser: Chrome
 
-  name:
-  message:
+  name: SyntaxError
+  message: SyntaxError: Illegal continue statement: no surrounding iteration statement
 
   callstack:
+    at HTMLIFrameElement.evaller.onload (study-with.js:40)
+    at Object.debugger (study-with.js:42)
+    at JavaScriptFE.studyWith (javascript-class.js:303)
+    at HTMLButtonElement.<anonymous> (javascript-class.js:149)
 
-  life cycle:
+  life cycle: creation phase
 
-  the mistake:
+  the mistake: it is not a loop to use continue statement (we use const, and it is not a user input).
 
-  the fix(es):
+  the fix(es): continue should be deleted.
 */
 
-const userName = 'wingdoz';
+const userName = "wingdoz";
 
 if (userName.length > 3) {
   console.log(userName);
-  continue;
 }
--- describing-errors/exercises/syntax-error/continue-mistake-2.js
+++ describing-errors/exercises/syntax-error/continue-mistake-2.js
@@ -1,24 +1,27 @@
-'use strict';
-
+"use strict";
 
 /*
-  browser:
+  browser: Chrome
 
-  name:
-  message:
+  name: SyntaxError
+  message: Undefined label 'x'
 
-  callstack:
+  callstack: 
+    at HTMLIFrameElement.evaller.onload (study-with.js:40)
+    at Object.debugger (study-with.js:42)
+    at JavaScriptFE.studyWith (javascript-class.js:303)
+    at HTMLButtonElement.<anonymous> (javascript-class.js:149)
 
-  life cycle:
+  life cycle: creation phase
 
-  the mistake:
+  the mistake: there is no label for this function x + y,
+  nothing to continue, it is not a loop with iterations.
 
-  the fix(es):
+  the fix(es): return instead continue, so that result of new variables like seven was displayed to the user.
 */
 
-
 const add = (x = 0, y = 0) => {
-  continue x + y;
+  return x + y; //error, mistake
 };
 
 const seven = add(3, 4);
