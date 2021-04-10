c93ef907278c4eb33428e2458bc866a497c27623
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-11 01:38:47 +0200

    name-mistakes

--- describing-errors/exercises/syntax-error/variable-name-mistake-1.js
+++ describing-errors/exercises/syntax-error/variable-name-mistake-1.js
@@ -1,20 +1,24 @@
-'use strict';
-
+"use strict";
 
 /*
-  browser:
+  browser: Chrome, Mozilla
 
-  name:
-  message:
+  name: SyntaxError
+  message: 
+  Mozilla: missing variable name
+  Chrome: Unexpected number
 
   callstack:
+    at HTMLIFrameElement.evaller.onload (study-with.js:40)
+    at Object.debugger (study-with.js:42)
+    at JavaScriptFE.studyWith (javascript-class.js:303)
+    at HTMLButtonElement.<anonymous> (javascript-class.js:149)
 
-  life cycle:
+  life cycle: creation
 
-  the mistake:
+  the mistake: name of variable can not start with number. 
 
-  the fix(es):
+  the fix(es): change "3" for "number3"
 */
 
-
-const 3 = 'three';
+const number3 = "three"; // error, mistake
--- describing-errors/exercises/syntax-error/variable-name-mistake-2.js
+++ describing-errors/exercises/syntax-error/variable-name-mistake-2.js
@@ -1,18 +1,24 @@
-'use strict';
+"use strict";
 
 /*
-  browser:
+  browser: Chrome, Mozilla
 
-  name:
-  message:
+  name: SyntaxError
+  message: 
+  Chrome: Identifier 'undefined' has already been declared
+  Mozilla: redeclaration of non-configurable global property undefined
 
   callstack:
+    at HTMLIFrameElement.evaller.onload (study-with.js:40)
+    at Object.debugger (study-with.js:42)
+    at JavaScriptFE.studyWith (javascript-class.js:303)
+    at HTMLButtonElement.<anonymous> (javascript-class.js:149)
 
-  life cycle:
+  life cycle: creation
 
-  the mistake:
+  the mistake: name of the variable is equal to js reserved word "undefined" for global property.
 
-  the fix(es):
+  the fix(es): change name for variable
 */
 
-const undefined = 'three';
+const number3 = "three"; //error, mistake
