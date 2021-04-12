da043566f931324854e657b0fa222b66aa1cae64
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-12 22:27:36 +0200

    comparing-and-asserting

--- isolate/03-comparing-and-asserting/exercises/2.js
+++ isolate/03-comparing-and-asserting/exercises/2.js
@@ -1,32 +1,31 @@
-'use strict';
+"use strict";
 
 // replace the _ to pass the asserts
 
-const comparison1 = 4 === '4';
-const test1 = comparison1 === _;
-console.assert(test1, 'Test 1');
+const comparison1 = 4 === "4";
+const test1 = comparison1 === false;
+console.assert(test1, "Test 1");
 
-const comparison2 = Infinity === 'Infinity';
-const test2 = comparison2 === _;
-console.assert(test2, 'Test 2');
+const comparison2 = Infinity === "Infinity";
+const test2 = comparison2 === false;
+console.assert(test2, "Test 2");
 
 const comparison3 = "x" === `x`;
-const test3 = comparison3 === _;
-console.assert(test3, 'Test 3');
+const test3 = comparison3 === true;
+console.assert(test3, "Test 3");
 
-const comparison4 = 'null' === _;
+const comparison4 = "null" === "null";
 const test4 = comparison4 === true;
-console.assert(test4, 'Test 4');
+console.assert(test4, "Test 4");
 
-const comparison5 = 'null' === null;
-const test5 = comparison5 === _;
-console.assert(test5, 'Test 5');
+const comparison5 = "null" === null;
+const test5 = comparison5 === false;
+console.assert(test5, "Test 5");
 
 const comparison6 = 0.0 === 0;
-const test6 = comparison6 === _;
-console.assert(test6, 'Test 6');
+const test6 = comparison6 === true;
+console.assert(test6, "Test 6");
 
 const comparison7 = null === undefined;
-const test7 = comparison7 === _;
-console.assert(test7, 'Test 7');
-
+const test7 = comparison7 === false;
+console.assert(test7, "Test 7");
