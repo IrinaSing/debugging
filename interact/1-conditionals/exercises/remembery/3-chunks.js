43c2990ea2111ceb241d2168a72ebfd816ca1e43
IrinaSing <irinasingh.mail@yandex.ru>
2021-04-27 21:48:55 +0200

    conditionals-remembery

--- interact/1-conditionals/exercises/remembery/3-chunks.js
+++ interact/1-conditionals/exercises/remembery/3-chunks.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 alert(`Remembery, the game of remembering.
 
@@ -7,29 +7,29 @@ next you will be prompted to remember one of them
 
 if you enter it exactly, you win!`);
 
-let input2 = '';
-let input1 = '';
-let input3 = '';
+let input2 = "";
+let input1 = "";
+let input3 = "";
 
-while (input1 === '' || input1 === null) {
-  input1 = prompt('enter your first secret phrase:');
+while (input1 === "" || input1 === null) {
+  input1 = prompt("enter your first secret phrase:");
 }
-while (input2 === '' || input2 === null) {
-  input2 = prompt('enter your second secret phrase:');
+while (input2 === "" || input2 === null) {
+  input2 = prompt("enter your second secret phrase:");
 }
-while (input3 === '' || input3 === null) {
-  input3 = prompt('enter your third secret phrase:');
+while (input3 === "" || input3 === null) {
+  input3 = prompt("enter your third secret phrase:");
 }
 
-alert('all saved! get ready to remember');
+alert("all saved! get ready to remember");
 
-const attempt = prompt('enter phrase number ', randomFrom1To3);
+const attempt = prompt("enter phrase number ", randomFrom1To3);
 
 const randomDecimal = Math.random();
 const decimalTimes3 = randomDecimal * 3;
 const randomFrom1To3 = Math.floor(decimalTimes3);
 
-let correctAnswer = '';
+let correctAnswer = "";
 if (randomFrom1To3 === 1) {
   correctAnswer = option1;
 } else if (randomFrom1To3 === 2) {
